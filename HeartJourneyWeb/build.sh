fa9ef85d9b91a2bf6f4ed20a52df3520b722c110
ronal <ronal@MRRECREATED-PC>
2026-09-24 18:32:04 -0400

    Add Cloudflare Pages build script

---
+++ HeartJourneyWeb/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+
+./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
+
+./dotnet/dotnet --version
+
+./dotnet/dotnet publish HeartJourneyWeb.csproj -c Release -o output
